edb250ec37c8d4715955947ac8820981afc8f5d9
Michael Stapelberg <michael@stapelberg.de>
2011-11-14 00:21:54 +0000

    Initial commit of i3build, a buildbot announce bot in Go

---
+++ Makefile
@@ -0,0 +1,7 @@
+include $(GOROOT)/src/Make.inc
+
+TARG=i3build
+GOFILES=\
+	src/i3build.go\
+
+include $(GOROOT)/src/Make.cmd
---
+++ src/i3build.go
@@ -0,0 +1,167 @@
+// vim:ts=4:sw=4
+// i3build - IRC bot to announce buildbot status
+// © 2011 Michael Stapelberg (see also: LICENSE)
+package main
+
+import irc "github.com/fluffle/goirc/client"
+import (
+	"fmt"
+	"http"
+	"log"
+	"json"
+	"os"
+	"flag"
+)
+
+var irc_channel *string = flag.String("channel", "#raumzeitlabor",
+	"In which channel this bot should be in")
+
+// Helper type: We first unmarshal the JSON into this type to get access to the
+// "event" string, then we decide which concrete type to unmarshal to.
+type minimalBuildbotEvent struct {
+	Event string
+}
+
+// Every event type has to implement this interface so that we can print it to
+// IRC.
+type BuildbotPrintableEvent interface {
+	AsChatLine() string
+}
+
+// A generic type to unmarshal to. Ev will either be set to some object
+// conforming to BuildbotPrintableEvent or nil.
+type BuildbotEvent struct {
+	Ev BuildbotPrintableEvent
+}
+
+// Type corresponding to the buildFinished event which buildbot sends.
+type BuildFinishedEvent struct {
+	Payload struct {
+		Build struct {
+			// We have to use the empty interface here because buildbot sends
+			// JSON values of varying type. They can be either string, number
+			// or null.
+			Properties [][]interface{}
+		}
+	}
+
+	buildername string
+	gitversion  string
+}
+
+func (o *BuildFinishedEvent) AsChatLine() string {
+	return fmt.Sprintf("Job [%s] finished for version %s",
+		o.buildername, o.gitversion)
+}
+
+func (o *BuildFinishedEvent) StoreKeyValue(key, value string) {
+	switch key {
+	case "buildername":
+		o.buildername = value
+	case "gitversion":
+		o.gitversion = value
+	}
+}
+
+func (o *BuildbotEvent) UnmarshalJSON(data []byte) os.Error {
+	var intermediate minimalBuildbotEvent
+	if err := json.Unmarshal(data, &intermediate); err != nil {
+		return err
+	}
+	if intermediate.Event == "buildFinished" {
+		event := new(BuildFinishedEvent)
+		if err := json.Unmarshal(data, event); err != nil {
+			return err
+		}
+
+		for _, property := range event.Payload.Build.Properties {
+			// Every property consists of a triple: key, value, something
+			if len(property) != 3 {
+				continue
+			}
+
+			// Try to convert key and value to string. If that fails, the
+			// property might still be valid, but we are only interested in the
+			// strings :).
+			key, key_ok := property[0].(string)
+			value, value_ok := property[1].(string)
+			if key_ok && value_ok {
+				event.StoreKeyValue(key, value)
+			}
+		}
+
+		o.Ev = event
+	}
+	return nil
+}
+
+func main() {
+	flag.Parse()
+
+	// Channel on which the HTTP handler sends lines to IRC.
+	to_irc := make(chan string)
+	quit := make(chan bool)
+
+	http.HandleFunc("/push_buildbot",
+		func(w http.ResponseWriter, r *http.Request) {
+			// Buildbot sends the packets URL-encoded.
+			if err := r.ParseForm(); err != nil {
+				log.Printf("Could not ParseForm: %s", err.String())
+			}
+
+			// Decode the JSON into BuildbotEvents and send them to IRC if
+			// appropriate.
+			var packets []BuildbotEvent
+			err := json.Unmarshal([]byte(r.Form.Get("packets")), &packets)
+			if err != nil {
+				log.Printf("Could not parse JSON: %s\n", err.String())
+			}
+			for _, event := range packets {
+				if event.Ev != nil {
+					to_irc <- event.Ev.AsChatLine()
+				}
+			}
+		})
+
+	// Handle HTTP requests in a different Goroutine.
+	go func() {
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			log.Fatal("ListenAndServer: ", err.String())
+		}
+	}()
+
+	c := irc.New("gobot", "ident", "real name")
+
+	c.AddHandler("connected",
+		func(conn *irc.Conn, line *irc.Line) { conn.Join(*irc_channel) })
+
+	c.AddHandler("disconnected",
+		func(conn *irc.Conn, line *irc.Line) { quit <- true })
+
+	log.Printf("Connecting...\n")
+	if err := c.Connect("irc.twice-irc.de"); err != nil {
+		log.Printf("Connection error: %s\n", err.String())
+	}
+
+	// program main loop
+	for {
+		select {
+		case err, ok := <-c.Err:
+			// If the error channel was closed, we lost the IRC connection.
+			if !ok {
+				break
+			}
+			if err != nil {
+				log.Printf("goirc error: %s", err.String())
+			}
+		case line, _ := <-to_irc:
+			c.Privmsg(*irc_channel, line)
+		case <-quit:
+			log.Println("Disconnected. Reconnecting...")
+			if err := c.Connect("irc.twice-irc.de"); err != nil {
+				log.Printf("Connection error: %s\n", err.String())
+			}
+		}
+	}
+	log.Fatalln("Fell out of the main loop?!")
+}

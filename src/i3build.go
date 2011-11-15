6e724a34e6515bbd274ce55897039a0d18672892
Michael Stapelberg <michael@stapelberg.de>
2011-11-15 22:55:09 +0000

    Revert "Send a PING every 250 seconds to avoid the 300 second timeout from disconnecting us"
    This reverts commit 7aff077755ab7476121a92aa288a1a645cb9ed5d.
    The issue has been fixed in revision 8fa5e56 of goirc

--- src/i3build.go
+++ src/i3build.go
@@ -11,7 +11,6 @@ import (
 	"json"
 	"os"
 	"flag"
-	"time"
 )
 
 var irc_channel *string = flag.String("channel", "#raumzeitlabor",
@@ -107,17 +106,6 @@ func main() {
 	// Channel on which the HTTP handler sends lines to IRC.
 	to_irc := make(chan string)
 	quit := make(chan bool)
-	// Workaround channel to send a PING every 250 seconds to avoid the 300
-	// second default timeout from disconnecting us.
-	ping := make(chan bool)
-
-	go func() {
-		for {
-			// sleep 250 seconds
-			time.Sleep(250 * 1e9)
-			ping <- true
-		}
-	}()
 
 	http.HandleFunc("/push_buildbot",
 		func(w http.ResponseWriter, r *http.Request) {
@@ -181,9 +169,6 @@ func main() {
 			if err := c.Connect("irc.twice-irc.de"); err != nil {
 				log.Printf("Connection error: %s\n", err.String())
 			}
-		case <-ping:
-			log.Println("Sending keepalive ping")
-			c.Raw("PING :keepalive")
 		}
 	}
 	log.Fatalln("Fell out of the main loop?!")
